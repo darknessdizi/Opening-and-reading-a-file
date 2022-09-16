b59085d703055e42d4c97a4f61f7825ece6c4dad
Remezov Dmitry <darknessdizi@gmail.com>
2022-09-17 00:16:50 +0800

    change file cook_book (create function for cook_book)

--- Task_1/cook_book.py
+++ Task_1/cook_book.py
@@ -1,21 +1,29 @@
-cook_book = {}
+def cook_book(new_file, encoding):
 
-with open('file_cook_book.txt', encoding='utf-8') as file:
-    for line in file:
-        if line != '\n':
-            name = line.rstrip('\n')
-            cook_book[name] = []
-            count = int(file.readline())
-            for i in range(count):
-                line = file.readline().rstrip('\n').split(' | ')
-                my_dict = {}
-                my_dict['ingredient_name'] = line[0]
-                my_dict['quantity'] = int(line[1])
-                my_dict['measure'] = line[2]
-                cook_book[name].append(my_dict)
+    '''Функция считывает файл в указанной кодировке и возвращает словарь 
+    с перечиисленными рецептами и ингридиентами. Файл должен быть стандартизирован.'''
 
-for i, y in cook_book.items():
-    print(i)
-    for z in y:
-        print(z)
-        
+    cook_book = {}
+
+    with open(new_file, encoding=encoding) as file:
+        for line in file:
+            if line != '\n':
+                name = line.rstrip('\n')
+                cook_book[name] = []
+                count = int(file.readline())
+                for i in range(count):
+                    line = file.readline().rstrip('\n').split(' | ')
+                    my_dict = {}
+                    my_dict['ingredient_name'] = line[0]
+                    my_dict['quantity'] = int(line[1])
+                    my_dict['measure'] = line[2]
+                    cook_book[name].append(my_dict)
+    return cook_book
+
+
+new_dict = cook_book('file_cook_book.txt', 'utf-8')
+
+for key, value in new_dict.items():
+    print(key)
+    for new_list in value:
+        print(new_list)
