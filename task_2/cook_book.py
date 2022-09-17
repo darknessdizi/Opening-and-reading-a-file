b20dd0883943dc56120a1d364358eaa2029dfc59
Remezov Dmitry <darknessdizi@gmail.com>
2022-09-17 20:39:43 +0800

    add fuction get_shop_list_by_dishes and finished task_2

--- task_2/cook_book.py
+++ task_2/cook_book.py
@@ -1,8 +1,10 @@
 def cook_book(new_file, encoding):
 
-    '''Функция считывает файл в указанной кодировке и возвращает словарь
-
-    с перечиисленными рецептами и ингридиентами. Файл должен быть стандартизирован.
+    '''Функция считывает файл в указанной кодировке и возвращает 
+    
+    словарь с перечиисленными рецептами и ингридиентами. Файл должен
+    
+    быть стандартизирован.
     
     '''
 
@@ -23,10 +25,42 @@ def cook_book(new_file, encoding):
                     cook_book[name].append(my_dict)
     return cook_book
 
+def get_shop_list_by_dishes(dishes, person_count):
 
-new_dict = cook_book('file_cook_book.txt', 'utf-8')
+    '''Функция на входе получает список блюд и колличество персон. 
+    
+    Возвращает словарь всех ингридиентов и их колличество необходимое
+    
+    для приготовления блюд из списка.
+    
+    '''
 
-for key, value in new_dict.items():
-    print(key)
-    for new_list in value:
-        print(new_list)
+    ingredients = {}
+    __cook_book = cook_book(FILE, ENCODING)
+    for name in dishes:
+        if name in __cook_book:
+            for element in __cook_book[name]:
+                if element.get('ingredient_name') in ingredients:
+                    dict_ingridient = ingredients[element.get('ingredient_name')]
+                    dict_ingridient['quantity'] += (
+                        element.get('quantity') * person_count
+                        )
+                else:
+                    ingredients[element.get('ingredient_name')] = (
+                        {'measure': element.get('measure'), 
+                        'quantity': element.get('quantity') * person_count}
+                        )
+    return ingredients
+
+
+ENCODING = 'utf-8'
+FILE = 'file_cook_book.txt'
+
+list_dishes = ['Запеченный картофель', 'Омлет']
+number_of_ingredients = get_shop_list_by_dishes(list_dishes, 2)
+
+for key, value in number_of_ingredients.items():
+     print(key, value)
+
+print('\n', number_of_ingredients)
+     
